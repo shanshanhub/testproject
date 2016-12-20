58405ae9afdc88b1cc48c9d4af7d94ebec6e6254
wanchuanlai <wanchuanlai@co-mall.com>
2016-12-20 15:14:21 +0800

    修改cache并发读写锁

--- cache/src/main/java/cache/CacheManager.java
+++ cache/src/main/java/cache/CacheManager.java
@@ -14,46 +14,50 @@ public class CacheManager {
 
     private final Cache cache = new MapCache();
     ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
-//
-//    public CacheManager(Cache cacheLevel1, Cache cacheLevel2) {
-//        this.cacheLevel1 = cacheLevel1;
-//        this.cacheLevel2 = cacheLevel2;
-//    }
 
     public Object get(String cacheName, String key, CacheDataFrom cacheDataFrom) {
-        //先暂时考虑一级缓存的事
-        //1,如果一级缓存可以直接读取,直接从一级缓存取
-        //2,如果一级缓存读取不到,从二级缓存读,
-        //从二级缓存读取到后,存到一级缓存
-        //这个时候要考虑并发的问题
+        //这个时候要考虑并发的问题,如果都在从缓存里面读取数据,刚好这个时候又过期了,这个时候,获取写锁,其它读锁都处于等待状态.
         //这里用读写锁考虑
+        // 读的时候上读锁,写的时候上写锁
         Object value = null;
         rwl.readLock().lock();
-        System.out.println(Thread.currentThread().getName() + " be ready to read data!");
+        System.out.println(Thread.currentThread().getName() + " be ready to read data! key:"+key);
+        try {
+            Thread.sleep((long) (Math.random() * 1000));
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         try {
             value = this.cache.get(key);
-            System.out.println(Thread.currentThread().getName() + " have read data :" + value);
-            if (value == null) {
-                rwl.readLock().unlock();
-                rwl.writeLock().lock();
-                System.out.println(Thread.currentThread().getName() + " be ready to write data!");
-                try {
-                    value = cacheDataFrom.putData();
-                    if (value != null) {
-                        this.cache.put(key, value);
-                    }
-//                        value = "aaaa";//测试读写锁用
-
-                    System.out.println(Thread.currentThread().getName() + " have  write data: " + value);
-                    rwl.readLock().lock();
-                } finally {
-                    rwl.writeLock().unlock();
-                }
-            }
+            System.out.println(Thread.currentThread().getName() + " have read key:"+key+" data :" + value);
         } finally {
             rwl.readLock().unlock();
 
         }
+        if (value == null) {
+            rwl.writeLock().lock();
+            try {
+                Thread.sleep((long) (Math.random() * 1000));
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " be ready to write key:"+key+" data!");
+            try {
+                value = cacheDataFrom.putData();
+                if (value != null) {
+                    this.cache.put(key, value);
+                }
+//                        value = "aaaa";//测试读写锁用
+
+                System.out.println(Thread.currentThread().getName() + " have  write key:"+key+" data: " + value);
+
+            } finally {
+                rwl.writeLock().unlock();
+            }
+        }
+
         return value;
 
     }
@@ -67,7 +71,22 @@ public class CacheManager {
                     while (true) {
                         cacheManager.get("cache", "AA", new CacheDataFrom() {
                             public Object putData() {
-                                return "aaaa";
+                                return (Math.random() * 1000);
+                            }
+                        });
+                    }
+                }
+
+            }.start();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            new Thread() {
+                public void run() {
+                    while (true) {
+                        cacheManager.get("cache", "BB", new CacheDataFrom() {
+                            public Object putData() {
+                                return (Math.random() * 1000);
                             }
                         });
                     }
